70becd49ae5c7bf78e9aa5c3338e6953ffa89cfb
Sai-Mohitha <ch.mohitha1999@gmail.com>
2024-03-22 10:25:24 +0000

    Routing-Practice

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+
+const Header = () => (
+  <ul className="nav-items-container">
+    <li>
+      <Link className="nav-links" to="/">
+        Home
+      </Link>
+    </li>
+    <li>
+      <Link className="nav-links" to="/about">
+        About
+      </Link>
+    </li>
+    <li>
+      <Link className="nav-links" to="/contact">
+        Contact
+      </Link>
+    </li>
+  </ul>
+)
+
+export default Header
